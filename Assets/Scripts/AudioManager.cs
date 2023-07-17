11745fed8097785528f87d5d1fb2bc2fd3a8bd0e
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-17 00:36:26 -0400

    Audio Manager Created

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOneShot(AudioClip audioClip)
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
