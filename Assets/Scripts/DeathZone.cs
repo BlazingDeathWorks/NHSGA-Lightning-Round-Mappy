b3a4979083a61befa993210ff4bb1143b31b5ed4
Future Inspire Games <shunsei.chan@gmail.com>
2023-07-19 23:36:37 -0400

    Update DeathZone.cs

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -11,5 +11,18 @@ public class DeathZone : MonoBehaviour
             PlayerLives.Instance.CanDie = true;
             PlayerLives.Instance.LoseLife();
         }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //Get points from the enemy and kill it
+            if (collision.gameObject.TryGetComponent(out BasicEnemyAI wolfer))
+            {
+                EnemySpawner.Instance?.RequestNewEnemy(wolfer);
+            }
+            if (collision.gameObject.TryGetComponent(out Paul paul))
+            {
+                EnemySpawner.Instance?.RequestNewEnemy(paul: paul);
+            }
+        }
     }
 }
