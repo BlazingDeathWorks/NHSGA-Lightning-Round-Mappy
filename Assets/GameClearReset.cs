ff2a049a2f5bde8a1f6ff267f15e1e6e0e3b9d94
DanaYu <yudana0703@gmail.com>
2023-07-20 18:15:25 -0400

    added game clear scene

---
+++ Assets/GameClearReset.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class GameClearReset : MonoBehaviour
+{
+    [SerializeField] private KeyCode input;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(input))
+        {
+            SceneManager.LoadScene(0);
+            AudioManagerGameOver.Instance.audioSource.Pause();
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            AudioManagerGameOver.Instance.audioSource.Pause();
+            Application.Quit();
+
+        }
+
+    }
+}
